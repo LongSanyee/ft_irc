46e9f41d926cd6ed58a5045a5896baefe018b170
Your Name <anouar.boukhmis@gmail.com>
2026-03-04 00:48:46 +0000

    join multiple

--- ft_irc/src/Commands.cpp
+++ ft_irc/src/Commands.cpp
@@ -3,6 +3,7 @@
 #include "../include/Server.hpp"
 #include "../include/irc.hpp"
 #include "../include/Command.hpp"
+#include <sstream>
 
 
 
@@ -72,6 +73,81 @@ void execute_pass(Command &cmd, Client &cl, Server &ser)
     cl.set_hasPass(true);
 }
 
+void join_one(std::string first, std::string second, Client &cl, Server &ser)
+{
+    if (first.empty() || first[0] != '#')
+    {
+        ser.sendmsg(cl.get_fd(), ":ircserv 403 " + cl.get_nickname() + " " + first + " :No such channel\r\n");
+        return;
+    }
+    std::map<std::string, Channel *>::iterator it = ser.getmap().find(first);
+    Channel *ch;
+    if (it != ser.getmap().end())
+    {
+        ch = it->second;
+        if (ch->getclients().find(cl.get_nickname()) != ch->getclients().end())
+            return;
+        if (ch->get_i() && cl.getclchannels()[first] != 1)
+        {
+            ser.sendmsg(cl.get_fd(), ":ircserv 473 " + cl.get_nickname() + " " + first + " :Cannot join channel (+i)\r\n");
+            return;
+        }
+        if (ch->get_k())
+        {
+            if (second.empty() || second != ch->get_key())
+            {
+                ser.sendmsg(cl.get_fd(), ":ircserv 475 " + cl.get_nickname() + " " + first + " :Cannot join channel (+k)\r\n");
+                return;
+            }
+        }
+        if (ch->get_l())
+        {
+            if ((int)ch->getclients().size() >= ch->get_limit())
+            {
+                ser.sendmsg(cl.get_fd(), ":ircserv 471 " + cl.get_nickname() + " " + first + " :Cannot join channel (+l)\r\n");
+                return;
+            }
+        }
+        cl.getclchannels()[first] = 0;
+    }
+    else
+    {
+        ch = new Channel();
+        ch->setname(first);
+        ser.getmap()[first] =ch;
+        cl.getclchannels()[first] = 2;
+    }
+    ch->addclient(cl.get_nickname(), &cl);
+    ch->sendtoall(ser, ":" + cl.get_nickname() + "!" + cl.get_username() + "@" + cl.gethost() + " JOIN " + first + "\r\n");
+     if (ch->gettopic().empty())
+        ser.sendmsg(cl.get_fd(), ":ircserv 331 " + cl.get_nickname() + " " + first + " :No topic is set\r\n");
+    else
+        ser.sendmsg(cl.get_fd(), ":ircserv 332 " + cl.get_nickname() + " " + first + " :" + ch->gettopic() + "\r\n");
+    std::string names;
+    std::map<std::string, Client *>::iterator cit = ch->getclients().begin();
+    for (; cit != ch->getclients().end(); ++cit)
+    {
+        if (cit->second->getclchannels()[first] == 2)
+            names += "@" + cit->first + " ";
+        else
+            names += cit->first + " ";
+    }
+    ser.sendmsg(cl.get_fd(), ":ircserv 353 " + cl.get_nickname() + " = " + first + " :" + names + "\r\n");
+    ser.sendmsg(cl.get_fd(), ":ircserv 366 " + cl.get_nickname() + " " + first + " :End of /NAMES list\r\n");
+}
+
+std::vector<std::string> split_cmd(std::string  str)
+{
+    std::stringstream first(str);
+    std::string s;
+    std::vector<std::string>tokens;
+    while (std::getline(first, s, ','))
+    {
+        tokens.push_back(s);
+    }
+    return tokens;
+}
+
 void execute_join(Command &cmd, Client &cl, Server &ser)
 {
     if (!cl.get_isregistred())
@@ -82,11 +158,6 @@ void execute_join(Command &cmd, Client &cl, Server &ser)
     if (cmd.getparams().empty())
     {
         ser.sendmsg(cl.get_fd(), ":ircserv 461 " + cl.get_nickname() + " JOIN :Not enough parameters\r\n");
-        return;
-    }
-    if (cmd.getparams()[0].empty() || cmd.getparams()[0][0] != '#')
-    {
-        ser.sendmsg(cl.get_fd(), ":ircserv 403 " + cl.get_nickname() + " " + cmd.getparams()[0] + " :No such channel\r\n");
         return;
     }
     if (cmd.getparams()[0] == "0")
@@ -108,60 +179,15 @@ void execute_join(Command &cmd, Client &cl, Server &ser)
         cl.getclchannels().clear();
         return;
     }
-    std::map<std::string, Channel *>::iterator it = ser.getmap().find(cmd.getparams()[0]);
-    Channel *ch;
-    if (it != ser.getmap().end())
+    std::vector<std::string> ch_names = split_cmd(cmd.getparams()[0]);
+    std::vector<std::string>ch_keys = split_cmd(cmd.getparams()[1]);
+    int i = 0;
+    while (!ch_names[i].empty())
     {
-        ch = it->second;
-        if (ch->getclients().find(cl.get_nickname()) != ch->getclients().end())
-            return;
-        if (ch->get_i() && cl.getclchannels()[cmd.getparams()[0]] != 1)
-        {
-            ser.sendmsg(cl.get_fd(), ":ircserv 473 " + cl.get_nickname() + " " + cmd.getparams()[0] + " :Cannot join channel (+i)\r\n");
-            return;
-        }
-        if (ch->get_k())
-        {
-            if (cmd.getparams().size() < 2 || cmd.getparams()[1] != ch->get_key())
-            {
-                ser.sendmsg(cl.get_fd(), ":ircserv 475 " + cl.get_nickname() + " " + cmd.getparams()[0] + " :Cannot join channel (+k)\r\n");
-                return;
-            }
-        }
-        if (ch->get_l())
-        {
-            if ((int)ch->getclients().size() >= ch->get_limit())
-            {
-                ser.sendmsg(cl.get_fd(), ":ircserv 471 " + cl.get_nickname() + " " + cmd.getparams()[0] + " :Cannot join channel (+l)\r\n");
-                return;
-            }
-        }
-        cl.getclchannels()[cmd.getparams()[0]] = 0;
+        join_one(ch_names[i], ch_keys[i], cl, ser);
+        i++;
     }
-    else
-    {
-        ch = new Channel();
-        ch->setname(cmd.getparams()[0]);
-        ser.getmap()[cmd.getparams()[0]] =ch;
-        cl.getclchannels()[cmd.getparams()[0]] = 2;
-    }
-    ch->addclient(cl.get_nickname(), &cl);
-    ch->sendtoall(ser, ":" + cl.get_nickname() + "!" + cl.get_username() + "@" + cl.gethost() + " JOIN " + cmd.getparams()[0] + "\r\n");
-     if (ch->gettopic().empty())
-        ser.sendmsg(cl.get_fd(), ":ircserv 331 " + cl.get_nickname() + " " + cmd.getparams()[0] + " :No topic is set\r\n");
-    else
-        ser.sendmsg(cl.get_fd(), ":ircserv 332 " + cl.get_nickname() + " " + cmd.getparams()[0] + " :" + ch->gettopic() + "\r\n");
-    std::string names;
-    std::map<std::string, Client *>::iterator cit = ch->getclients().begin();
-    for (; cit != ch->getclients().end(); ++cit)
-    {
-        if (cit->second->getclchannels()[cmd.getparams()[0]] == 2)
-            names += "@" + cit->first;
-        else
-            names += cit->first;
-    }
-    ser.sendmsg(cl.get_fd(), ":ircserv 353 " + cl.get_nickname() + " = " + cmd.getparams()[0] + " :" + names + "\r\n");
-    ser.sendmsg(cl.get_fd(), ":ircserv 366 " + cl.get_nickname() + " " + cmd.getparams()[0] + " :End of /NAMES list\r\n");
+    
 }
 
 
@@ -235,16 +261,25 @@ void execute_ping(Command &cmd, Client &cl, Server &ser)
     ser.sendmsg(cl.get_fd(), pong);
 }
 
+void execute_kick(Command &cmd, Client &cl, Server &ser)
+{
+    if (cl.)
+}
+
+
 void execute_cmd(Command &cmd, Client &cl, Server &ser)
 {
     if (cmd.getcmd() == "PASS") {execute_pass(cmd,cl,ser);}
-    else if (cmd.getcmd() == "NICK") { execute_nick(cmd,cl,ser);}
+    else if (cmd.getcmd() == "NICK") {execute_nick(cmd,cl,ser);}
     else if (cmd.getcmd() == "USER") {  }
     else if (cmd.getcmd() == "QUIT") {execute_quit(cmd, cl, ser);}
     else if (cmd.getcmd() == "PING") {execute_ping(cmd, cl, ser);}
-    else if (cmd.getcmd() == "JOIN") {  }
-    else if (cmd.getcmd() == "PRIVMSG") { execute_privmsg(cmd,cl,ser);}
-    else { }
+    else if (cmd.getcmd() == "JOIN") {execute_join(cmd, cl, ser); }
+    else if (cmd.getcmd() == "PRIVMSG") {execute_privmsg(cmd,cl,ser);}
+    else if (cmd.getcmd() == "KICK" ) {}
+    else if (cmd.getcmd() == "INVITE" ){}
+    else if (cmd.getcmd() == "TOPIC" ){}
+    else if (cmd.getcmd() == "MODE") {}
 
 }
 
